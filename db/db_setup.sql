40093261fb5020975c6671f11294b9e3f108d75c
gchu <gchu647@gmail.com>
2018-08-03 12:36:21 -1000

    created products table

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -14,3 +14,17 @@ CREATE TABLE users (
 
 INSERT INTO users(email, password)
 VALUES ('superfigher@street.com', 'boomshaka');
+
+
+CREATE TABLE products (
+  id  SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255),
+  description TEXT,
+  inventory INTEGER NOT NULL,
+  price DECIMAL(8, 2) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+INSERT INTO products(title, description, inventory, price)
+VALUES ('bananas', 'keeps the doctors away', 10, 2);
