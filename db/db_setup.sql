2616dc55a8691a01db07d08e926ac9b44b54ede2
gchu <gchu647@gmail.com>
2018-08-03 15:58:49 -1000

    Get cart based on userId works

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -28,3 +28,22 @@ CREATE TABLE products (
 
 INSERT INTO products(title, description, inventory, price)
 VALUES ('bananas', 'keeps the doctors away', 10, 2);
+
+CREATE TABLE cart (
+  id  SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  products_id INTEGER REFERENCES products(id),
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+-- user adds something to the cart
+INSERT INTO cart(user_id, products_id)
+VALUES (2, 2);
+
+-- get all the products from user id 6
+SELECT users.email ,products.*
+FROM cart
+INNER JOIN users ON users.id = cart.user_id
+INNER JOIN products ON products.id = cart.products_id
+WHERE users.id = 6;
--- routes/cart.js
+++ routes/cart.js
@@ -1,8 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/knex');
 
-router.get('/', (req, res) => {
-  res.send('I am a cart!');
+// gets products from the cart of one user
+router.get('/:user_id', (req, res) => {
+  const userId = parseInt(req.params.user_id);
+
+  knex.raw(
+`SELECT users.email ,products.*
+FROM cart
+INNER JOIN users ON users.id = cart.user_id
+INNER JOIN products ON products.id = cart.products_id
+WHERE users.id = ? ;`, [userId])
+    .then(cart => {
+      if(!cart ||!cart.rowCount) {
+        throw new Error('Cart not found');
+      }
+      
+      res.json(cart.rows);
+    })
+    .catch(err => res.status(400).send(err.message));
+});
+
+// make user id add an product
+router.post('/:user_id/:products_id', (req, res) => {
+  const userId = parseInt(req.params.user_id);
+  const productsId = parseInt(req.params.products_id);
+
+  console.log('userId ', userId);
+  console.log('productsId ', productsId);
+
+  knex.raw(
+`INSERT INTO cart(user_id, products_id)
+ VALUES (?, ?);`, [userId, productsId])
+    .then(cart => {
+      if(!cart || !cart.rowCount) {
+        throw new Error('Cart not added');
+      }
+      
+      res.send(`user${userId} added product${productsId} into cart`);
+    })
+    .catch(err => res.status(400).send(err.message));
 });
 
 module.exports = router;
